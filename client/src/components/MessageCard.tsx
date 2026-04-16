3bcfedff2aad8fd785b259d85fffb7adbcd63484
wlewis0991 <wlewis1990@gmail.com>
2026-04-16 14:43:30 -0700

    Message card displays messages but not liked to user table to display user name

--- client/src/components/MessageCard.tsx
+++ client/src/components/MessageCard.tsx
@@ -1,6 +1,6 @@
 import type { Message } from "../types/types"
 
-function MessageCard({ user_id, created_at, message }: Message){
+function MessageCard({ user_id, created_at, message,likes }: Message){
     return (
         <div className="post-card">
             <div className="post-meta">
@@ -8,6 +8,7 @@ function MessageCard({ user_id, created_at, message }: Message){
                 <span className="date">{new Date(created_at).toLocaleDateString()}</span>
             </div>
             <h3>{message}</h3>
+            <h3>{likes}</h3>
         </div>
     )
 }
